f5d27fecd0bacb061a31294056070a0397d95189
a.rahmati <a.rahmati@kianiranian.com>
2023-05-24 12:09:21 +0330

    feat: update

--- public/main.js
+++ public/main.js
@@ -1,47 +1,44 @@
 const Clientsoket = io();
 //agh in var emit kardim bayd on var on konim agh onvar on kardim bayd inja emit konim
 //1 test ro bgir k on var to server emit cardim(   soketio.emit("test", { message: "hello" });)
-Clientsoket.on("test", function(data) {
-    // console.log(data);
-});
-//2 emit bad dar server on
-Clientsoket.emit("testmani", { number: 20 })
-    //
 
-$("#loginBtn").on("click", function(name) {
+
+$("#loginBtn").on("click", function (name) {
     event.preventDefault();
     username = $("#username").val();
     Clientsoket.emit("login", { username });
-    Clientsoket.on("himself", function(data) {
+    Clientsoket.on("himself", function (data) {
         console.log(data.message);
     });
     $("#loginContainer").css("opacity", "0");
     $("#chatForm").css("opacity", "1");
     $("#chatHistory").css("opacity", "1");
 });
-$("#messagenpm").on("click", function(name) {
+$("#messagenpm").on("click", function (name) {
     event.preventDefault();
     let message = $("#mass").val();
-    console.log(message);
     Clientsoket.emit('newmassage', { message, username });
 
-    $("#chatHistory").append(`<div> 
+    $("#chatHistory").append(`<div style=margin-left:auto> 
     <div class="you">YOU</div>
+    <img src="./icon.png" alt="Avatar" style="width:50px">
     <div>${message}</div>
     </div>`)
 });
 
-Clientsoket.on("broadcast", function(data) {
-    console.log(data);
+Clientsoket.on("broadcast", function (data) {
+    console.log("5555555");
+    console.log(data.name);
     $("#chatHistory").append(`<div> 
     <div class="other">${data.name}</div>
+    <img src="/w3images/bandmember.jpg" alt="Avatar" style="width:100%;">
     <div>${data.mati}</div>
     </div>`)
 
 })
 
 
-Clientsoket.on("newuser", function(data) {
+Clientsoket.on("newuser", function (data) {
     console.log(data.namenew);
 
 })
